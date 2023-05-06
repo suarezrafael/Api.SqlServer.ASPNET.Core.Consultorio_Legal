764904b63a80fff33f7d7a94b7a20d60588eea62
Rafael Vieira Suarez <rafael.suarez@compasso.com.br>
2023-05-06 00:10:23 -0300

    Adicionar arquivos de projeto.

---
+++ CL.WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,51 @@
+﻿using CL.Core.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CL.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(
+                new List<Cliente> 
+            { 
+                new Cliente { Id= 1, Nome ="Rafael", DataNascimento = DateTime.Now}, 
+                new Cliente { Id= 1, Nome ="Rafael", DataNascimento = DateTime.Now} 
+            });
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
